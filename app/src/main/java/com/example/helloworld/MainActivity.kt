a79658b6a2563bf3b64bba03f3546251305702c4
Will-Josh Njeumi <willjosh.njeumi@gmail.com>
2023-01-10 22:09:48 -0500

    Test commit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.helloButton)
         button.setOnClickListener {
             Log.v("Hello world", "Button clicked!")
-            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Hello to you!", Toast.LENGTH_SHORT).show()
         }
     }
 }
